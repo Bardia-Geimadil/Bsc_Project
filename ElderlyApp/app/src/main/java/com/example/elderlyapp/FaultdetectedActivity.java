d768b538a5e9eb2ef7f3180cc3954f837a2c9a84
Bardia-Geimadil <bardiageimadil@gmail.com>
2021-07-24 17:38:23 +0430

    AsyncTask added

---
+++ ElderlyApp/app/src/main/java/com/example/elderlyapp/FaultdetectedActivity.java
@@ -0,0 +1,14 @@
+package com.example.elderlyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FaultdetectedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faultdetected);
+    }
+}
--- ElderlyApp/app/src/main/java/com/example/elderlyapp/work_activity.java
+++ ElderlyApp/app/src/main/java/com/example/elderlyapp/work_activity.java
@@ -1,15 +1,21 @@
 package com.example.elderlyapp;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -28,7 +34,7 @@ public class work_activity extends AppCompatActivity implements View.OnClickList
 
     public final static int REQ_BT_ENABLE = 1212 ;
     static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-    StringBuilder sb = new StringBuilder();
+
     BluetoothDevice hc05;
     BluetoothSocket btSocket = null;
 
@@ -36,6 +42,15 @@ public class work_activity extends AppCompatActivity implements View.OnClickList
     boolean faultDetected;
 
     BluetoothAdapter bluetoothAdapter;
+
+
+
+    /////////////////////////////Variables/////////////////////////////////
+    StringBuilder sb = new StringBuilder();
+    int time_to_show;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,12 +67,10 @@ public class work_activity extends AppCompatActivity implements View.OnClickList
          bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
          hc05 = bluetoothAdapter.getRemoteDevice("78:D8:5D:10:1D:0C"); // this is the unique mac of my hc_0c
-        String aaa = "aaaaa";
-        beatRate_tv.setText(aaa);
+
 
          heartImage.setOnClickListener(this);
 
-         //connectBluetooth();
 
          faultDetected = false;
 
@@ -66,18 +79,25 @@ public class work_activity extends AppCompatActivity implements View.OnClickList
 
     }
 
+
     public void communicate(){
 
 
         try {
             OutputStream outputStream = btSocket.getOutputStream();
             outputStream.write(49);
-            Toast.makeText(this , "Sending Data ....." , Toast.LENGTH_LONG).show();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+
+
         while (!faultDetected){
+
+            time_to_show++;
+
+            if(time_to_show == 60)
+                time_to_show=0;
 
             InputStream inputStream = null;
             try {
@@ -119,21 +139,7 @@ public class work_activity extends AppCompatActivity implements View.OnClickList
 
               if(btSocket.isConnected()) {
 
-
-//                  final Handler handler = new Handler();
-//                  handler.postDelayed(new Runnable() {
-//                      @Override
-//                      public void run() {
-//                          communicate();
-//                      }
-//                  }, 2000);
                   connectionFlag = true;
-                  communicate();
-//                  new AlertDialog.Builder(work_activity.this)
-//                          .setTitle("Connected!")
-//                          .setIcon(R.drawable.greencheck)
-//                          .setCancelable(true)
-//                          .setMessage("Device Connected Successfully!").show();
               }
 
             } catch (IOException e) {
@@ -142,6 +148,140 @@ public class work_activity extends AppCompatActivity implements View.OnClickList
             counter++;
             System.out.println("Trying to connect");
         } while (!btSocket.isConnected() && counter < 10);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+        if(view.getId() == heartImage.getId()) {
+
+            new communicateAsyncTask().executeOnExecutor(communicateAsyncTask.THREAD_POOL_EXECUTOR);
+        }
+
+    }
+
+    class communicateAsyncTask extends AsyncTask<String , String , String>
+    {
+
+        @Override
+        protected String doInBackground(String... objects) {
+
+
+            try {
+                OutputStream outputStream = btSocket.getOutputStream();
+                outputStream.write(49);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+
+            while (!faultDetected){
+
+                InputStream inputStream = null;
+                try {
+                    inputStream = btSocket.getInputStream();
+                    inputStream.skip(inputStream.available());
+                    // Toast.makeText(this , "Receiving Data ....." , Toast.LENGTH_LONG).show();
+                    for (int i = 0; i < 12; i++) {
+
+                        byte b = (byte) inputStream.read();
+                        System.out.println((char) b);
+                        sb.append((char) b);
+//                Toast.makeText(MainActivity.this , b , Toast.LENGTH_SHORT).show();
+
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                //beatRate_tv.setText(sb.toString());
+
+                if(sb.toString().contains("Bardia")) {
+                    faultDetected = true;
+                    startActivity(new Intent(work_activity.this , FaultdetectedActivity.class));
+                }
+
+                time_to_show++;
+
+                if(time_to_show == 60)
+                {
+                    time_to_show = 0;
+                    publishProgress(sb.toString());
+                }
+
+                sb.setLength(0);
+
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+
+                 beatRate_tv.setText(values[0]);
+
+
+        }
+    }
+
+
+    class connectionAsyncTask extends AsyncTask{
+
+
+        @Override
+        protected Object doInBackground(Object[] objects) {
+
+            connectBluetooth();
+
+            return null;
+        }
+
+    @Override
+    protected void onPostExecute(Object o) {
+
+        new AlertDialog.Builder(work_activity.this)
+                .setTitle("Connected!")
+                .setIcon(R.drawable.greencheck)
+                .setCancelable(true)
+                .setMessage("Device Connected Successfully!").show();
+    }
+}
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = new MenuInflater(this);
+        menuInflater.inflate(R.menu.work_menu , menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getTitle().equals("Connect"))
+        {
+
+            new connectionAsyncTask().executeOnExecutor(connectionAsyncTask.THREAD_POOL_EXECUTOR);
+
+//           Thread thread = new Thread(new Runnable() {
+//               @Override
+//               public void run() {
+//
+//                   connectBluetooth();
+//               }
+//           });
+//           Toast.makeText(work_activity.this , "Connecting ... " , Toast.LENGTH_SHORT).show();
+//           thread.start();
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
@@ -178,11 +318,4 @@ public class work_activity extends AppCompatActivity implements View.OnClickList
     }
 
 
-    @Override
-    public void onClick(View view) {
-
-        if(view.getId() == heartImage.getId())
-            connectBluetooth();
-
-    }
 }
